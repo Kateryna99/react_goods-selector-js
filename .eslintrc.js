99fd06ef5f5ad46e51d33229a20b4da94115fa7f
Katya <katya.stelmakhova@gmail.com>
2024-07-29 16:49:52 +0200

    Fixing bugs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,4 @@
 module.exports = {
   extends: ['@mate-academy/eslint-config-react', 'plugin:cypress/recommended'],
-  rules: {
-    'prettier/prettier': 'off',
-    "react/no-array-index-key": "off"
-  },
+  rules: {},
 };
--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,19 @@ export const goods = [
 
 export const App = () => {
   const [selectedGood, setSelectedGood] = useState('Jam');
+  const isGoodSelected = good => selectedGood === good;
 
   return (
     <main className="section container">
       {selectedGood.length ? (
         <h1 className="title is-flex is-align-items-center">
           {selectedGood} is selected
-          <button onClick={()=> setSelectedGood('') }
-                  data-cy="ClearButton" type="button" className="delete ml-3" />
+          <button
+            onClick={() => setSelectedGood('')}
+            data-cy="ClearButton"
+            type="button"
+            className="delete ml-3"
+          />
         </h1>
       ) : (
         <h1 className="title is-flex is-align-items-center">
@@ -32,30 +37,39 @@ export const App = () => {
         </h1>
       )}
 
-
       <table className="table">
         <tbody>
-        {goods.map((item, index) => (
-          <tr key={index} data-cy="Good"
-              className={selectedGood === item
-                && 'has-background-success-light'}
-          >
-            <td>
-              {selectedGood === item ? (
-                  <button onClick={()=> setSelectedGood('')}
-                          data-cy="RemoveButton" type="button"
-                          className="button is-info">
+          {goods.map(good => (
+            <tr
+              data-cy="Good"
+              className={isGoodSelected(good) && 'has-background-success-light'}
+            >
+              <td>
+                {isGoodSelected(good) ? (
+                  <button
+                    onClick={() => setSelectedGood('')}
+                    data-cy="RemoveButton"
+                    type="button"
+                    className="button is-info"
+                  >
                     -
-                  </button>)
-                : (
-                  <button onClick={()=> setSelectedGood(item)}
-                          data-cy="AddButton" type="button" className="button">+
-                  </button>)}
-
-            </td>
-            <td data-cy="GoodTitle" className="is-vcentered">{item}</td>
-          </tr>
-        ))}
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => setSelectedGood(good)}
+                    data-cy="AddButton"
+                    type="button"
+                    className="button"
+                  >
+                    +
+                  </button>
+                )}
+              </td>
+              <td data-cy="GoodTitle" className="is-vcentered">
+                {good}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </main>
